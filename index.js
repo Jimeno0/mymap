410dc2ab199ec860ff8a857cf6900f228dfabcc5
Jimeno0 <carlospj@icloud.com>
2016-02-23 11:58:21 +0100

    fixing buttons commit

--- index.js
+++ index.js
@@ -15,9 +15,27 @@ Mad.addEventListener('click', function(){
 
 
 var Mad = document.getElementById('btnMad');
-Mad.addEventListener('click',function(){map.centerAndZoom([-3.688510, 40.453010],9);})
+Mad.addEventListener('click',function(){
+
+  map.centerAndZoom([-3.688510, 40.453010],5);
+  var delay = 1000;
+  setTimeout(function(){
+  map.centerAndZoom([-3.688510, 40.453010],10);
+     }, delay); 
+ })
 
 
 var Bcn = document.getElementById('btnBcn');
-Bcn.addEventListener('click',function(){map.centerAndZoom([2.154007, 41.390205],10);})
+Bcn.addEventListener('click',function(){
 
+map.centerAndZoom([-3.688510, 40.453010],5);
+  var delay = 1000;
+  setTimeout(function(){
+  map.centerAndZoom([2.154007, 41.390205],10);
+     }, delay); 
+ })
+
+
+
+
+//map.centerAndZoom([2.154007, 41.390205],10);})
