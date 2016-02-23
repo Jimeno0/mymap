6af21d01319b45131d9cdcbcd28c344c0ae10c79
Jimeno0 <carlospj@icloud.com>
2016-02-23 20:53:20 +0100

    base maps buttons commit

--- index.js
+++ index.js
@@ -33,3 +33,55 @@ map.centerAndZoom([-3.688510, 40.453010],5);
   map.centerAndZoom([-3.529895, 40.371853],16);
      }, delay);
  })
+
+
+var BtnStrts = document.getElementById('BtnStrts');
+BtnStrts.addEventListener('click',function(){
+
+  map.setBasemap('streets');
+ });
+
+
+var BtnTopo = document.getElementById('BtnTopo');
+BtnTopo.addEventListener('click',function(){
+
+map.setBasemap('topo');
+ });
+
+
+var BtnSat = document.getElementById('BtnSat');
+BtnSat.addEventListener('click',function(){
+
+map.setBasemap('satellite');
+ });
+
+
+var BtnOcean = document.getElementById('BtnOcean');
+BtnOcean.addEventListener('click',function(){
+
+map.setBasemap('oceans');
+ });
+ 
+ 
+
+
+
+
+ /*
+
+<script>
+  
+require(["dojo/dom", "dojo/domReady!"], function() {
+
+var BtnOcean = document.getElementById('Strts');
+BtnOcean.addEventListener('click',function(){
+
+map.setBasemap('oceans');
+ });
+});
+
+</script>
+
+
+
+*/
